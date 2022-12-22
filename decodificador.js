068f0ddba06cefdc611a90dceed70c8199d7b637
Vitor Fialho <vitor.fialho4@gmail.com>
2022-12-22 14:48:50 -0300

    Todos os arquivos do projeto decodificador de texto

---
+++ decodificador.js
@@ -0,0 +1,40 @@
+let textoResultado;
+let botaoCriptografar = document.getElementById("criptografar");
+let botaoDescriptografa = document.getElementById("descriptografar");
+let botaoCopia = document.getElementById("copiar");
+let botaoLimpa = document.getElementById("limpar");
+
+function criptografa(){
+    let textoDigitado = document.getElementById("texto-digitado").value;
+    textoResultado = textoDigitado.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    document.getElementById("resultado").innerHTML = textoResultado;
+}
+
+function descriptografa(){
+    let textoDigitado = document.getElementById("texto-digitado").value;
+    textoResultado = textoDigitado.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.getElementById("resultado").innerHTML = textoResultado;
+}
+
+function copiaTexto(){
+    let texto = document.getElementById("resultado").textContent;
+    navigator.clipboard.writeText(texto);
+}
+
+function limpaTexto(){
+    document.getElementById("resultado").textContent = "";
+}
+
+botaoCriptografar.onclick = criptografa;
+botaoDescriptografa.onclick = descriptografa;
+botaoCopia.onclick = copiaTexto;
+botaoLimpa.onclick = limpaTexto;
+
+
+
+
+
+
+
+
+
